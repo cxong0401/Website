52b7df75d037685767aa539bdad52d4a2c38666b
cxong0401 <149515748+cxong0401@users.noreply.github.com>
2023-10-31 15:53:37 +0000

    Add files via upload

---
+++ section1.js
@@ -0,0 +1,255 @@
+//questions
+const question_bank = [];
+question_bank[0]="Sarah loves to read, and today she's in a bookstore. She picks up a book she's been wanting for a while, and it costs RM10. Then, she finds a beautiful pen for RM2.75. Thus, she should pay the total amount of RM12.75 to the cashier.";
+question_bank[1]="David is shopping for a new outfit for a special event. He chooses a stylish shirt that costs RM25. Along with that, he picks out a matching tie for RM8.50. Total cost of David's new look is RM33.50";
+question_bank[2]="Tom decides to have a nice meal at his favorite restaurant. He orders a delicious meal for RM12. To quench his thirst, he also orders a drink for RM3.25. The total amount of RM15.25 where Tom needs to pay for his meal and drink.";
+question_bank[3]="Emma is at the toy store, and she's excited to get a new toy. She selects a toy she's been eyeing for a while, and it costs RM15.75. To satisfy her sweet tooth, she grabs some candy for RM0.60. RM16.35 is the total expense for Emma's treats.";
+question_bank[4]="Lisa decides to treat herself to some pampering. She goes to the salon and gets a stylish haircut for RM18.50. She also gets a manicure for RM5.  Lisa should pay RM23.50 for her salon services";
+question_bank[5]="Jack is a gamer, and he's been waiting for a new video game to release. It costs RM30. While he's playing, he gets hungry and buys a snack for RM4.25. RM34.25 was the total amount Jack needs to pay for his entertainment and snack";
+question_bank[6]="Olivia plans a movie night out. She buys a movie ticket for RM9.75 and can't resist getting a box of popcorn for RM2.50. The total cost of Olivia's movie night is RM12.25";
+question_bank[7]="Alex wants to start cycling, so he buys a bicycle for RM85. Safety is essential, so he also purchases a helmet for RM12. Alex's total expenditure for his new bike and helmet is RM97";
+question_bank[8]="Sophie wants to update her wardrobe. She chooses a stylish dress for RM45. To complete the look, she buys a pair of beautiful shoes for RM18.75. How much does Sophie need to pay for her new outfit which total amount is RM63.75";
+question_bank[9]="James stops by a café for a coffee break. He orders a cup of coffee for RM4.50 and a delicious sandwich for RM6.25. RM10.75 was the total cost of James' meal at the café";
+question_bank[10]="Emma, an adventurous foodie, strolled through the bustling fruit market. She picked up 3 juicy apples for RM1.50 each and 4 plump bananas for RM2.25. Her total came to RM10, a fair price for a delightful fruit adventure.";
+question_bank[11]="David, an avid reader, embarked on a literary journey at the cozy neighborhood bookstore. He chose 2 gripping novels for RM15 each and a colorful magazine for RM4.50. As he left the store, he held RM34.50 worth of knowledge in his hands.";
+question_bank[12]="Sophie, a fashion enthusiast, decided it was time for a wardrobe makeover. She snagged 2 trendy T-shirts for RM12 each and a pair of stylish jeans for RM25. Her total bill came to RM49, a small price to pay for a fresh new look.";
+question_bank[13]="Mike, a tech enthusiast, ventured into the high-tech store. He secured a sleek smartphone for RM80 and a protective phone case for RM5. His quest for the latest gadgets cost him a total of RM85.";
+question_bank[14]="Oliver, an outdoor enthusiast, embarked on an expedition into the wilderness. He invested in a sturdy tent for RM40, a reliable camping stove for RM20, and a cozy sleeping bag for RM15. The price of adventure totaled RM75.";
+question_bank[15]="Lisa, a coffee connoisseur, sought solace in a cozy café. She ordered a fragrant cappuccino for RM4 and a buttery croissant for RM3.50. Her escape from the world cost her a total of RM7.50.";
+question_bank[16]="Ben, with a green thumb, ventured into a magical garden center. He collected 5 packets of flower seeds for RM2 each and a shiny new watering can for RM5. His green paradise had a price tag of RM15.";
+question_bank[17]="Lucy, a budding artist, embarked on an artistic journey. She filled her cart with 3 high-quality sketchbooks for RM6 each and a vibrant set of paints for RM10. Her artistic journey came to a total of RM28.";
+question_bank[18]="Tom, a pet lover, set out for a pet store adventure. He acquired a bag of nutritious dog food for RM8.50 and a sturdy leash for RM12. His adventure into pet care cost him a total of RM20.50.";
+question_bank[19]="Sophia, a diligent student, embarked on an expedition for essential office supplies. She acquired 2 packs of pens for RM5 each and a sleek notebook for RM3. Her academic expedition totaled RM13.";
+question_bank[20]="Anna decided to stock up on art supplies, grabbing paints for RM45.20 and two sketchbooks for RM26.75, totaling RM71.95.";
+question_bank[21]="Mark made his garden bloom with a beautiful bouquet of flowers for RM18.90 and added three potted plants for RM36.50, coming to a total of RM55.40.";
+question_bank[22]="Lucy went to her favorite bookstore and selected a novel for RM29.95. She also couldn't resist picking up two magazines for RM7.20, making her total expenditure RM37.15.";
+question_bank[23]="Tom made a quick grocery run, picking up fresh fruits for RM14.30, crisp vegetables for RM12.50, and some snacks for RM8.75. In the end, his total grocery bill amounted to RM35.55.";
+question_bank[24]="Sophie wanted the best for her furry friend, so she bought dog food for RM23.60 and a new leash for RM9.90. Her total pet supply cost was RM33.50.";
+question_bank[25]="David treated himself to a night at the movies, buying a ticket for RM13.50. He also indulged in a large popcorn for RM8.75, bringing his total expenditure to RM22.25.";
+question_bank[26]="Emma found a stunning dress for RM42.80 and paired it with a lovely scarf for RM8.75. Her clothing shopping added up to RM51.55.";
+question_bank[27]="Oliver needed some new tech accessories, so he picked up a phone case for RM15.30 and a wireless charger for RM19.50, resulting in a total electronics cost of RM34.80.";
+question_bank[28]="Nina went on a stationery shopping spree, grabbing a set of notebooks for RM12.80 and stocking up on pens for RM5.40. Her stationery expenses totaled RM18.20.";
+question_bank[29]="James enjoyed a leisurely coffee break, savoring a cup of coffee and indulging in a pastry for RM4.75. His coffee break was a delightful RM9.50 in total.";
+question_bank[30]="Rachel, an avid tennis player, invested in her sport, purchasing a tennis racket for RM55.60 and stocking up on tennis balls for RM8.40. Her sports gear expenses came to RM64.00.";
+
+
+
+
+//answers
+const answer_bank = [];
+answer_bank[0]=12.75;
+answer_bank[1]=33.50;
+answer_bank[2]=15.25;
+answer_bank[3]=16.35;
+answer_bank[4]=23.50;
+answer_bank[5]=34.25;
+answer_bank[6]=12.25;
+answer_bank[7]=97.00;
+answer_bank[8]=63.75;
+answer_bank[9]=10.75;
+answer_bank[10]=10.00;
+answer_bank[11]=34.50;
+answer_bank[12]=49.00;
+answer_bank[13]=85.00;
+answer_bank[14]=75.00;
+answer_bank[15]=7.50;
+answer_bank[16]=15.00;
+answer_bank[17]=28.00;
+answer_bank[18]=20.50;
+answer_bank[19]=13.00;
+answer_bank[20]=71.95;
+answer_bank[21]=55.40;
+answer_bank[22]=37.15;
+answer_bank[23]=35.55;
+answer_bank[24]=33.50;
+answer_bank[25]=22.25;
+answer_bank[26]=51.55;
+answer_bank[27]=34.80;
+answer_bank[28]=18.20;
+answer_bank[29]=9.50;
+answer_bank[30]=64.00;
+
+
+
+
+function generateOneToTwenty() {
+    const random_number = Math.floor(Math.random() * 11) * 1; // Random integer between 0 and 10
+    return random_number;
+}
+
+let questionNumber = generateOneToTwenty();
+
+function updateQuestion() {
+    const questionElement = document.getElementById('questions');
+    questionElement.textContent = question_bank[questionNumber];
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    questionNumber = generateOneToTwenty();
+    updateQuestion();
+
+    // Clear the selected notes/coins and result message when the page loads
+    selectedNotesCoins.length = 0;
+    updateSelectedNotesCoins();
+    
+    const result = document.getElementById('result');
+    result.textContent = '';
+    result.style.color = '';
+});
+
+// Initialize an array to track selected notes/coins
+const selectedNotesCoins = [];
+
+
+function updateSelectedNotesCoins() {
+    const selectedArea = document.getElementById('selected-notes-coins');
+    selectedArea.innerHTML = '';
+
+    let totalAmount = 0;
+
+    for (let i = 0; i < selectedNotesCoins.length; i++) {
+        const noteCoin = selectedNotesCoins[i];
+        const img = document.createElement('img');
+        img.src = noteCoin.src;
+        img.alt = noteCoin.alt;
+        img.style.maxWidth = '150px';
+        img.style.maxHeight = '70px';
+        img.setAttribute('data-index', i); // Set a data attribute to store the index
+
+        // Add a click event listener to remove the selected note/coin
+        img.addEventListener('click', () => {
+            selectedNotesCoins.splice(i, 1); // Remove the item from the array
+            updateSelectedNotesCoins(); // Update the displayed notes/coins
+        });
+
+        selectedArea.appendChild(img);
+
+        totalAmount += noteCoin.value;
+    }
+    totalAmount = parseFloat(totalAmount.toFixed(2)); // Round to 2 decimal places
+
+}
+
+
+const bgm = document.getElementById('bgm');
+const originalBgmVolume = bgm.volume;
+
+
+function checkAnswer() {
+    let totalAmount = 0;
+
+    for (const noteCoin of selectedNotesCoins) {
+        totalAmount += noteCoin.value;
+    }
+    totalAmount = parseFloat(totalAmount.toFixed(2)); // Round to 2 decimal places
+
+    const result = document.getElementById('result');
+
+    if (totalAmount == answer_bank[questionNumber]) {
+        result.textContent = 'Correct! You matched the money amount.';
+        result.style.color = 'green';
+        
+        // Play the correct sound
+        const correctSound = document.getElementById('correct-sound');
+        correctSound.play();
+    } else {
+        result.textContent = 'Incorrect. Try again.';
+        result.style.color = 'red';
+        
+        // Play the incorrect sound
+        const incorrectSound = document.getElementById('incorrect-sound');
+        incorrectSound.play();
+    }
+}
+
+function checkAnswer() {
+    let totalAmount = 0;
+
+    for (const noteCoin of selectedNotesCoins) {
+        totalAmount += noteCoin.value;
+    }
+    totalAmount = parseFloat(totalAmount.toFixed(2)); // Round to 2 decimal places
+
+    const result = document.getElementById('result');
+
+    if (totalAmount == answer_bank[questionNumber]) {
+        result.textContent = 'Correct! You matched the money amount.';
+        result.style.color = 'green';
+
+        // Lower the BGM volume temporarily
+        bgm.volume = originalBgmVolume * 0.1;
+
+        // Play the correct sound
+        const correctSound = document.getElementById('correct-sound');
+        correctSound.play();
+
+        // Restore the BGM volume to its original level after a delay (adjust the delay as needed)
+        setTimeout(() => {
+            bgm.volume = originalBgmVolume;
+        }, 4000); 
+    } else {
+        result.textContent = 'Incorrect. Try again.';
+        result.style.color = 'red';
+
+        // Lower the BGM volume temporarily
+        bgm.volume = originalBgmVolume * 0.1;
+
+        // Play the incorrect sound
+        const incorrectSound = document.getElementById('incorrect-sound');
+        incorrectSound.play();
+
+        // Restore the BGM volume to its original level after a delay (adjust the delay as needed)
+        setTimeout(() => {
+            bgm.volume = originalBgmVolume;
+        }, 11000); 
+    }
+}
+
+
+
+
+
+// Event listeners for notes/coins
+const notesCoins = document.querySelectorAll('.note-coin');
+for (const noteCoin of notesCoins) {
+    noteCoin.addEventListener('click', () => {
+        selectedNotesCoins.push({
+            src: noteCoin.src,
+            alt: noteCoin.alt,
+            value: parseFloat(noteCoin.getAttribute('data-value')),
+        });
+
+        updateSelectedNotesCoins();
+    });
+}
+
+// Event listener for the "Check Answer" button
+const checkButton = document.getElementById('check-answer');
+checkButton.addEventListener('click', checkAnswer);
+
+// Function to handle the "Next Question" button click event
+function nextQuestion() {
+    // Generate a new random question number
+    questionNumber = generateOneToTwenty();
+
+    // Update the displayed question
+    updateQuestion();
+
+    // Clear the selected notes/coins
+    selectedNotesCoins.length = 0;
+    updateSelectedNotesCoins();
+
+    // Clear the result message
+    const result = document.getElementById('result');
+    result.textContent = '';
+    result.style.color = '';
+}
+
+// Event listener for the "Next Question" button
+const nextQuestionButton = document.getElementById('next-question');
+nextQuestionButton.addEventListener('click', nextQuestion);
+
+
+
+
